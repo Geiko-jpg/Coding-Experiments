5966038572d88b2ba1695767a9978d83d0bfb4cb
Geiko-jpg <57441023+Geiko-jpg@users.noreply.github.com>
2020-10-04 16:33:04 +0800

    timeCalc

---
+++ timeCalc/Time_Main.java
@@ -0,0 +1,12 @@
+package com.kyle.timeCalc;
+
+public class Time_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculation a = new Calculation();
+		
+		a.displayCalculation();
+	}
+
+}
