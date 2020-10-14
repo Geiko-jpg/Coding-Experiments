5f133afa40344b14afeeb5e85817b5c94a57568f
Geiko-jpg <57441023+Geiko-jpg@users.noreply.github.com>
2020-10-14 13:07:10 +0800

    Experiment

---
+++ Experiment/src/com/java/firstExperiment/MySqlTest.java
@@ -0,0 +1,15 @@
+
+/**
+ * @author: kyle
+ */
+
+public class MySqlTest{
+    public static void main(String[] args){
+            try{
+                // Creating MySql database connection
+                String myDriver = "org.gjt.mm.mysql.Driver";
+            }catch(Exception e){
+
+            }
+    }
+}
