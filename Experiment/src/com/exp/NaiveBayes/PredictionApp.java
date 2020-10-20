844f2a179ade17aa2ab9aa7bf24907fd684ec9f8
Geiko-jpg <57441023+Geiko-jpg@users.noreply.github.com>
2020-10-20 14:30:20 +0800

    Experiment

--- Experiment/src/com/exp/NaiveBayes/PredictionApp.java
+++ Experiment/src/com/exp/NaiveBayes/PredictionApp.java
@@ -1,21 +1,65 @@
 package com.exp.NaiveBayes;
 
-import java.util.Scanner;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EtchedBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 public class PredictionApp {
-	private static Scanner scan = new Scanner(System.in);
+	private JFrame primaryFrame;
+	private JTable table;
+	private JPanel primePanel;
+	private JScrollPane scrollTable;
+	
+	public PredictionApp(){
+		primaryFrame = new JFrame();
+		primaryFrame.setSize(1200, 700);
+		primaryFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		primaryFrame.setTitle("Naive Bayes App");
+		primaryFrame.getContentPane().setLayout(null);
+		primaryFrame.setResizable(false);
+		
+		// - - > OPEN TABLE VIEW
+		dataTable();
+		
+		primaryFrame.setVisible(true);
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("INPUT FNAME: ");
-		String fName = scan.nextLine();
+		new PredictionApp();
+	}
+	
+	public void dataTable() {
+		Object columnsData[] = new Object[5];
+		columnsData[0] = "AGE";
+		columnsData[1] = "STUDENT";
+		columnsData[2] = "INCOME";
+		columnsData[3] = "CREDIT RATING";
+		columnsData[4] = "BUYS COMPUTER";
 		
-		System.out.print("INPUT LNAME: ");
-		String lName = scan.nextLine();
+		TableModel tableData = new DefaultTableModel(columnsData, 0);
+		table = new JTable(tableData);
 		
-		PersonDatabase adapter = new PersonDatabase();
-		adapter.WriteToDatabase(fName, lName);
-		adapter.readDatabase();
+		scrollTable = new JScrollPane(table);
+		scrollTable.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+		scrollTable.setLocation(10, 15);
+		scrollTable.setSize(950, 633);
+		primaryFrame.getContentPane().add(scrollTable);
+		
+		DefaultTableCellRenderer centerRend = new DefaultTableCellRenderer();
+		centerRend.setHorizontalAlignment(JLabel.CENTER);
+		table.getColumnModel().getColumn(0).setCellRenderer(centerRend);
+		table.getColumnModel().getColumn(1).setCellRenderer(centerRend);
+		table.getColumnModel().getColumn(2).setCellRenderer(centerRend);
+		table.getColumnModel().getColumn(3).setCellRenderer(centerRend);
+		table.getColumnModel().getColumn(4).setCellRenderer(centerRend);
 	}
 
 }
